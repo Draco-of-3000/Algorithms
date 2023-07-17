7380c94c847aaf6ed9231b092521900277537504
Draco <kevin.tenisson@gmail.com>
2023-07-17 07:37:19 -0700

    Initialized head node and added append method

---
+++ linked_list.rb
@@ -0,0 +1,19 @@
+class Linkedlist
+    attr_reader :head
+
+    def initialize
+        @head = nil
+    end
+
+    def append(value)
+        if @head.nil?
+            @head = Node.new(value)
+        else 
+            current_node = @head 
+            while current_node.next_node
+                current_node = current_node.next_node
+            end
+            current_node.next_node = Node.new(value)
+        end
+    end
+end

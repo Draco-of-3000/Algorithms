372bc7ae27b8b4615c84f803a8546df38c214130
Draco <kevin.tenisson@gmail.com>
2023-07-25 02:54:40 -0700

    Added a private helper method for build tree

--- balanced_bst.rb
+++ balanced_bst.rb
@@ -26,4 +26,19 @@ class Tree
 
         @root = build_tree_helper(sorted_arr, 0, sorted_arr.length - 1)
     end
+
+    private 
+
+    def build_tree_helper(arr, start_index, end_index)
+        return nil if start_index > end_index
+
+        mid_index = (start_index + end_index) / 2
+
+        new_node = Node.new(arr[mid_index]) # Create a new node with the middle element as data
+
+        new_node.left = build_tree_helper(arr, start_index, mid_index - 1)
+        new_node.right = build_tree_helper(arr, mid_index + 1, end_index)
+
+        new_node
+    end
 end

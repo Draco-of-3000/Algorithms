596c2f37740e32055c4eb54646644fef008fc5bf
Draco <kevin.tenisson@gmail.com>
2023-07-26 10:59:54 -0700

    Modified driver script to visualize bst properly

--- driver_script.rb
+++ driver_script.rb
@@ -4,31 +4,46 @@ require_relative 'balanced_bst'
 random_array = Array.new(15) { rand(1..100) }
 tree = Tree.new(random_array)
 
+tree.elegant_print
+
 # Confirm that the tree is balanced
-puts "Tree is balanced? #{tree.balanced?}"  # Should print 'true' if balanced
+puts "Your Binary Search Tree is #{tree.balanced? ? 'balanced' : 'not balanced'}."
 
 # Print out all elements in level, pre, post, and in order
-puts "Level order: #{tree.level_order.to_a}"
-puts "Preorder: #{tree.preorder.to_a}"
-puts "Postorder: #{tree.postorder.to_a}"
-puts "Inorder: #{tree.inorder.to_a}"
+puts "Level order traversal:"
+tree.level_order { |node| print "#{node.data} " }
+puts "\nPreorder traversal:"
+tree.preorder { |node| print "#{node.data} " }
+puts "\nInorder traversal:"
+tree.inorder { |node| print "#{node.data} " }
+puts "\nPostorder traversal:"
+tree.postorder { |node| print "#{node.data} " }
 
 # Unbalance the tree by adding several numbers > 100
-tree.insert(150)
-tree.insert(200)
-tree.insert(250)
+5.times do
+    a = rand(100..150)
+    tree.insert(a)
+    puts "Inserted #{a} to tree."
+end
 
 # Confirm that the tree is unbalanced
-puts "Tree is balanced? #{tree.balanced?}"  # Should print 'false' if unbalanced
+puts "\nYour Binary Search Tree is #{tree.balanced? ? 'balanced' : 'not balanced'}."
+puts "Rebalancing tree..."
 
 # Balance the tree by calling #rebalance
 tree.rebalance
 
+tree.elegant_print
+
 # Confirm that the tree is balanced after rebalancing
-puts "Tree is balanced? #{tree.balanced?}"  # Should print 'true' if balanced
+puts "Your Binary Search Tree is #{tree.balanced? ? 'balanced' : 'not balanced'}."
 
 # Print out all elements in level, pre, post, and in order after rebalancing
-puts "Level order (after rebalancing): #{tree.level_order.to_a}"
-puts "Preorder (after rebalancing): #{tree.preorder.to_a}"
-puts "Postorder (after rebalancing): #{tree.postorder.to_a}"
-puts "Inorder (after rebalancing): #{tree.inorder.to_a}"
+puts "Level order traversal:"
+tree.level_order { |node| print "#{node.data} " }
+puts "\nPreorder traversal:"
+tree.preorder { |node| print "#{node.data} " }
+puts "\nInorder traversal:"
+tree.inorder { |node| print "#{node.data} " }
+puts "\nPostorder traversal:"
+tree.postorder { |node| print "#{node.data} " }
